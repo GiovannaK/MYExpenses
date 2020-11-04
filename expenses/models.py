007f34a9b0837c0432c9d795dd288866f7652861
Giovanna Cunha <giovannakelli@gmail.com>
2020-11-04 20:36:53 -0300

    expenses models

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Expenses(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.CharField(max_length=800, verbose_name="Descrição")
+    quantity = models.DecimalField(max_digits=19, decimal_places=2, verbose_name="Quantidade")
+    date = models.DateField(verbose_name="Data")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Gasto'
+        verbose_name_plural = 'Gastos'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nome da categoria")
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.name)}'
+            self.slug = slug
--- expenses/views.py
+++ expenses/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'expenses/landing_page.html')
+    return render(request, 'expenses/expenses.html')
