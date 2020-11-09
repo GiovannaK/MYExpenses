6c14722521b63097ee0a4e1c0b2a36819512b7dd
Giovanna Cunha <giovannakelli@gmail.com>
2020-11-08 22:58:47 -0300

    Add clean method to forms

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,5 +1,7 @@
-from django.forms import ModelForm, TextInput, DateTimeInput
+from django.forms import ModelForm, DateTimeInput
 from .models import Expenses
+from django.forms import ValidationError
+
 
 class ExpenseCreationForm(ModelForm):
     class Meta:
@@ -8,3 +10,20 @@ class ExpenseCreationForm(ModelForm):
         widgets = {
             'date': DateTimeInput(attrs={'type': 'date'})
         }
+
+
+    def clean_title(self):
+        super(ExpenseCreationForm, self).clean()
+        title_cleaned = self.cleaned_data.get('title')
+        
+        title_bd = Expenses.objects.filter(title__icontains=title_cleaned).exists()
+
+        if title_bd:
+            raise ValidationError('Este título já existe!')
+
+        return title_cleaned    
+
+
+        
+
+
