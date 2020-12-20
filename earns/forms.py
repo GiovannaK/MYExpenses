c28d67bb846955241c5cd7dd960779e433f28d4d
Giovanna Cunha <giovannakelli@gmail.com>
2020-12-20 16:05:05 -0300

    adding template logic to reports

--- earns/forms.py
+++ earns/forms.py
@@ -18,5 +18,5 @@ class EarnsUpdateForm(ModelForm):
         model = Earns
         fields = ['category', 'title', 'description', 'date', 'quantity', 'currency', 'long_term']
         widgets = {
-            'date': DateTimeInput(attrs={'type': 'date'})
+            'date': DateTimeInput(attrs={'type': 'date', 'class': 'datepicker'})
         }
