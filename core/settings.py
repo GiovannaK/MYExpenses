f559744b7c7a4e122949ea33babd543bcd3aef87
Giovanna Cunha <giovannakelli@gmail.com>
2021-01-15 02:28:12 -0300

    expose postgres port

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['myexpensesweb.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['myexpensesweb.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
