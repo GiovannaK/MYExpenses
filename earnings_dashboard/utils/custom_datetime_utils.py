3052bc407c90b2cb70c6185fa1bb1e5dc6ce61af
Giovanna Cunha <giovannakelli@gmail.com>
2021-02-02 20:12:51 -0300

    fix current_month dateutils

--- earnings_dashboard/utils/custom_datetime_utils.py
+++ earnings_dashboard/utils/custom_datetime_utils.py
@@ -1,22 +1,24 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import calendar
 
 current_year = datetime.now().year
 current_month = datetime.now().month
 current_month_date = datetime.now()
+last_day = calendar.monthrange(current_year, current_month)[1]
 
 five_years_ago = current_year - 5
 last_three_months = current_month_date - relativedelta(months=3)
 last_year = current_year - 1
 
-month_start = relativedelta(year=current_year, month=current_month, day=1)
-month_end = relativedelta(year=current_year, month=current_month, day=31)
+month_start = datetime(year=current_year, month=current_month, day=1)
+month_end = datetime(year=current_year, month=current_month, day=last_day)
 
-start_date = relativedelta(year=current_year, month=1, day=1)
-end_date = relativedelta(year=current_year, month=12, day=31)
+start_date = datetime(year=current_year, month=1, day=1)
+end_date = datetime(year=current_year, month=12, day=31)
 
-initial_year = relativedelta(year=five_years_ago, month=1, day=1)
-final_year = relativedelta(year=current_year, month=12, day=31)
+initial_year = datetime(year=five_years_ago, month=1, day=1)
+final_year = datetime(year=current_year, month=12, day=31)
 
-initial_last_year = relativedelta(year=last_year, month=1, day=1)
-final_last_year = relativedelta(year=last_year, month=12, day=31)
+initial_last_year = datetime(year=last_year, month=1, day=1)
+final_last_year = datetime(year=last_year, month=12, day=31)
