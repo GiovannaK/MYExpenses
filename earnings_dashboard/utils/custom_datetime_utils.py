e35e2689f85ab827c2eb484254ad822b8e0ffede
Giovanna Cunha <giovannakelli@gmail.com>
2021-02-02 19:20:29 -0300

    fixing dates using relativedelta

--- earnings_dashboard/utils/custom_datetime_utils.py
+++ earnings_dashboard/utils/custom_datetime_utils.py
@@ -9,14 +9,14 @@ five_years_ago = current_year - 5
 last_three_months = current_month_date - relativedelta(months=3)
 last_year = current_year - 1
 
-month_start = datetime(year=current_year, month=current_month, day=1)
-month_end = datetime(year=current_year, month=current_month, day=31)
+month_start = relativedelta(year=current_year, month=current_month, day=1)
+month_end = relativedelta(year=current_year, month=current_month, day=31)
 
-start_date = datetime(year=current_year, month=1, day=1)
-end_date = datetime(year=current_year, month=12, day=31)
+start_date = relativedelta(year=current_year, month=1, day=1)
+end_date = relativedelta(year=current_year, month=12, day=31)
 
-initial_year = datetime(year=five_years_ago, month=1, day=1)
-final_year = datetime(year=current_year, month=12, day=31)
+initial_year = relativedelta(year=five_years_ago, month=1, day=1)
+final_year = relativedelta(year=current_year, month=12, day=31)
 
-initial_last_year = datetime(year=last_year, month=1, day=1)
-final_last_year = datetime(year=last_year, month=12, day=31)
+initial_last_year = relativedelta(year=last_year, month=1, day=1)
+final_last_year = relativedelta(year=last_year, month=12, day=31)
